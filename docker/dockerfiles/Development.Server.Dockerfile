4cde230729c87b8c02d5a5acdda00795e20c4348
zhurak-v <zhurak.github@gmail.com>
2025-11-11 21:51:29 +0200

    Update

--- docker/dockerfiles/Development.Server.Dockerfile
+++ docker/dockerfiles/Development.Server.Dockerfile
@@ -1,4 +1,4 @@
-FROM eclipse-temurin:21-jdk-jammy
+FROM eclipse-temurin:21-jre-jammy
 
 WORKDIR /app
 
@@ -8,7 +8,7 @@ EXPOSE 7777
 EXPOSE 9090
 
 ENV SPRING_PROFILES_ACTIVE=development
-
 ENV APPLICATION_PORT=7777
+ENV JAVA_OPTS="-Xms1536m -Xmx2048m -XX:+UseG1GC -XX:MaxGCPauseMillis=200"
 
-ENTRYPOINT ["java", "-jar", "application.jar", "--spring.profiles.active=${SPRING_PROFILES_ACTIVE}"]
+ENTRYPOINT ["sh", "-c", "java $JAVA_OPTS -jar application.jar --spring.profiles.active=${SPRING_PROFILES_ACTIVE}"]
--- server/src/main/java/com/flashcards/server/common/utils/http/configuration/AsyncConfiguration.java
+++ server/src/main/java/com/flashcards/server/common/utils/http/configuration/AsyncConfiguration.java
@@ -4,15 +4,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
 
-import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 @Configuration
 @EnableAsync
 public class AsyncConfiguration
 {
-    @Bean(name = "taskExecutor")
-    public Executor virtualThreadExecutor() {
+    @Bean(name = "taskExecutor", destroyMethod = "close")
+    public ExecutorService virtualThreadExecutor() {
         return Executors.newVirtualThreadPerTaskExecutor();
     }
 }
