0b0e122c8b1ee4f815d1d5b14d53842560dde892
JaamalL <vitkt20@gmail.com>
2025-11-13 19:31:34 +0200

    chore(flashcards): Delete unnecessery imports

--- server/src/main/java/com/flashcards/server/flashcards/infrastructure/repository/FlashcardRepository.java
+++ server/src/main/java/com/flashcards/server/flashcards/infrastructure/repository/FlashcardRepository.java
@@ -3,12 +3,10 @@ package com.flashcards.server.flashcards.infrastructure.repository;
 import com.flashcards.server.common.data.repository.BaseRepository;
 import com.flashcards.server.flashcards.core.entities.Flashcard;
 import com.flashcards.server.flashcards.core.ports.repository.FlashcardRepositoryPort;
-import com.flashcards.server.profile.core.entites.Profile;
 import jakarta.persistence.NoResultException;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 
 public class FlashcardRepository extends BaseRepository<Flashcard> implements FlashcardRepositoryPort {
