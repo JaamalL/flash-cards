fd638338e0bc5e9e87a2b46ae64692e7341a2c13
JaamalL <vitkt20@gmail.com>
2025-11-13 19:29:59 +0200

    chore(flashcards): Implement findByUserId method in FlashcardRepository adapter

--- server/src/main/java/com/flashcards/server/flashcards/infrastructure/repository/FlashcardRepository.java
+++ server/src/main/java/com/flashcards/server/flashcards/infrastructure/repository/FlashcardRepository.java
@@ -1,4 +1,30 @@
 package com.flashcards.server.flashcards.infrastructure.repository;
 
-public class FlashcardRepository {
+import com.flashcards.server.common.data.repository.BaseRepository;
+import com.flashcards.server.flashcards.core.entities.Flashcard;
+import com.flashcards.server.flashcards.core.ports.repository.FlashcardRepositoryPort;
+import com.flashcards.server.profile.core.entites.Profile;
+import jakarta.persistence.NoResultException;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public class FlashcardRepository extends BaseRepository<Flashcard> implements FlashcardRepositoryPort {
+    public FlashcardRepository() {
+        super(Flashcard.class);
+    }
+
+    @Override
+    public List<Flashcard> findByUserId(UUID userId) {
+        try {
+            return em.createQuery(
+                            "SELECT f FROM Flashcard f WHERE f.userId = :userId", type)
+                    .setParameter("userId", userId)
+                    .getResultList();
+        } catch (NoResultException ex) {
+            return new ArrayList<>();
+        }
+    }
 }
