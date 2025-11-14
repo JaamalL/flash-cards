59b206dfc83fd74dc0ef86fee91416fa03b8708f
JaamalL <vitkt20@gmail.com>
2025-11-14 12:03:46 +0200

    feat(flashcards): Add createFlashcard method

--- server/src/main/java/com/flashcards/server/flashcards/core/ports/services/CreateFlashcardPort.java
+++ server/src/main/java/com/flashcards/server/flashcards/core/ports/services/CreateFlashcardPort.java
@@ -1,4 +1,8 @@
 package com.flashcards.server.flashcards.core.ports.services;
 
+import com.flashcards.server.flashcards.core.dto.CreateFlashcardDTO;
+import com.flashcards.server.flashcards.core.entities.Flashcard;
+
 public interface CreateFlashcardPort {
+    Flashcard createFlashcard(CreateFlashcardDTO createFlashcardDTO);
 }
