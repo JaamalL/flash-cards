c485c805b18e97535a1eb581993cac98fb7a9854
JaamalL <vitkt20@gmail.com>
2025-11-15 23:07:45 +0200

    refactor(flashcards): Add 'userId' as separate argument (not as part of CreateFlashcardDTO)

--- server/src/main/java/com/flashcards/server/flashcards/core/ports/services/CreateFlashcardPort.java
+++ server/src/main/java/com/flashcards/server/flashcards/core/ports/services/CreateFlashcardPort.java
@@ -3,7 +3,8 @@ package com.flashcards.server.flashcards.core.ports.services;
 import com.flashcards.server.flashcards.core.dto.CreateFlashcardDTO;
 
 import java.util.Map;
+import java.util.UUID;
 
 public interface CreateFlashcardPort {
-    Map<String, String> createFlashcard(CreateFlashcardDTO createFlashcardDTO);
+    Map<String, String> createFlashcard(CreateFlashcardDTO createFlashcardDTO, UUID userId);
 }
