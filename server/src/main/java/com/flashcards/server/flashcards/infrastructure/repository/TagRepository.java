fa06ae0d0f336c40e66f92b7aee2a272ff47b756
JaamalL <vitkt20@gmail.com>
2025-11-16 15:45:44 +0200

    chore(flashcards): Implement TagRepositoryPort methods in TagRepository adapter

--- server/src/main/java/com/flashcards/server/flashcards/infrastructure/repository/TagRepository.java
+++ server/src/main/java/com/flashcards/server/flashcards/infrastructure/repository/TagRepository.java
@@ -1,4 +1,44 @@
 package com.flashcards.server.flashcards.infrastructure.repository;
 
-public class TagRepository {
+import com.flashcards.server.common.data.repository.BaseRepository;
+import com.flashcards.server.flashcards.core.entities.Tag;
+import com.flashcards.server.flashcards.core.ports.repository.TagRepositoryPort;
+import jakarta.persistence.NoResultException;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Repository
+public class TagRepository extends BaseRepository<Tag> implements TagRepositoryPort {
+    public TagRepository() {
+        super(Tag.class);
+    }
+
+    @Override
+    public List<Tag> findByUserId(UUID userId) {
+        try {
+            return em.createQuery("SELECT t FROM Tag t WHERE t.userId = :userId", type)
+                    .setParameter("userId", userId)
+                    .getResultList();
+        } catch (NoResultException ex) {
+            return new ArrayList<>();
+        }
+    }
+
+    @Override
+    public List<Tag> findAllById(List<UUID> tagIds) {
+        if (tagIds == null || tagIds.isEmpty()) {
+            return new ArrayList<>();
+        }
+
+        try {
+            return em.createQuery("SELECT t FROM Tag t WHERE t.id IN :ids", type)
+                    .setParameter(":ids", tagIds)
+                    .getResultList();
+        } catch (NoResultException ex) {
+            return new ArrayList<>();
+        }
+    }
 }
