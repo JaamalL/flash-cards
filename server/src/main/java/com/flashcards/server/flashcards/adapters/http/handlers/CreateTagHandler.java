a8a17e688ca844e3b39aea0fe6ee64c4423a71ef
JaamalL <vitkt20@gmail.com>
2025-11-16 15:52:00 +0200

    feat(flashcards): Add CreateTagHandler to handle tag creation

--- server/src/main/java/com/flashcards/server/flashcards/adapters/http/handlers/CreateTagHandler.java
+++ server/src/main/java/com/flashcards/server/flashcards/adapters/http/handlers/CreateTagHandler.java
@@ -1,4 +1,26 @@
 package com.flashcards.server.flashcards.adapters.http.handlers;
 
+import com.flashcards.server.flashcards.core.dto.CreateTagDTO;
+import com.flashcards.server.flashcards.core.ports.services.CreateTagPort;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+import java.util.UUID;
+
+@Component
 public class CreateTagHandler {
+    private final CreateTagPort createTagPort;
+
+    public CreateTagHandler(CreateTagPort createTagPort) {
+        this.createTagPort = createTagPort;
+    }
+
+    public ResponseEntity<Map<String, String>> handle(Jwt jwt, CreateTagDTO createTagDTO) {
+        return ResponseEntity.ok(createTagPort.createTag(
+                createTagDTO,
+                UUID.fromString(jwt.getSubject())
+        ));
+    }
 }
