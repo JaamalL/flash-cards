d12c0b87f1e77a677b1dfbb14865e337ac7d05df
JaamalL <vitkt20@gmail.com>
2025-11-16 13:35:29 +0200

    chore(flashcards): Delete unnecessary imports in CreateFlashcardDTO

--- server/src/main/java/com/flashcards/server/flashcards/core/dto/CreateFlashcardDTO.java
+++ server/src/main/java/com/flashcards/server/flashcards/core/dto/CreateFlashcardDTO.java
@@ -3,8 +3,6 @@ package com.flashcards.server.flashcards.core.dto;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
-import java.util.UUID;
-
 public record CreateFlashcardDTO(
         @NotNull(message = "TextQuestion cannot be null")
         String textQuestion,
