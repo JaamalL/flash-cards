23d3bff6795375178efb5988a3bd598b4adf3ca0
JaamalL <vitkt20@gmail.com>
2025-11-16 22:38:14 +0200

    feat(flashcards): Add validation to tagIds list in CreateFlashcardDTO

--- server/src/main/java/com/flashcards/server/flashcards/core/dto/CreateFlashcardDTO.java
+++ server/src/main/java/com/flashcards/server/flashcards/core/dto/CreateFlashcardDTO.java
@@ -1,6 +1,7 @@
 package com.flashcards.server.flashcards.core.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 
 import java.util.List;
@@ -16,5 +17,7 @@ public record CreateFlashcardDTO(
         @NotBlank(message = "Answer cannot be blank")
         String answer,
 
+        @NotNull(message = "TagIds list cannot be null")
+        @NotEmpty(message = "TagIds list cannot be empty")
         List<UUID> tagIds
 ) {}
