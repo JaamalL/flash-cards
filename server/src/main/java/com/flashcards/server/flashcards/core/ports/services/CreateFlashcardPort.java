14b488fde4728998a6b98c69a51e358a8ee63377
JaamalL <vitkt20@gmail.com>
2025-11-16 23:07:48 +0200

    refactor(flashcards): Change createFlashcard return value from Map<String, String> to FlashcardDTO in CreateFlashcardPort

--- server/src/main/java/com/flashcards/server/flashcards/core/ports/services/CreateFlashcardPort.java
+++ server/src/main/java/com/flashcards/server/flashcards/core/ports/services/CreateFlashcardPort.java
@@ -1,10 +1,11 @@
 package com.flashcards.server.flashcards.core.ports.services;
 
 import com.flashcards.server.flashcards.core.dto.CreateFlashcardDTO;
+import com.flashcards.server.flashcards.core.dto.FlashcardDTO;
 
 import java.util.Map;
 import java.util.UUID;
 
 public interface CreateFlashcardPort {
-    Map<String, String> createFlashcard(CreateFlashcardDTO createFlashcardDTO, UUID userId);
+    FlashcardDTO createFlashcard(CreateFlashcardDTO createFlashcardDTO, UUID userId);
 }
