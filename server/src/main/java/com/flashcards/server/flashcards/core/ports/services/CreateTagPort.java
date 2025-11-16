da7e6c29e45463584c9553f53a43766e2fcf7ebb
JaamalL <vitkt20@gmail.com>
2025-11-16 23:36:45 +0200

    refactor(flashcards): Change createTag return value to TagDTO in CreateTagPort

--- server/src/main/java/com/flashcards/server/flashcards/core/ports/services/CreateTagPort.java
+++ server/src/main/java/com/flashcards/server/flashcards/core/ports/services/CreateTagPort.java
@@ -1,10 +1,11 @@
 package com.flashcards.server.flashcards.core.ports.services;
 
 import com.flashcards.server.flashcards.core.dto.CreateTagDTO;
+import com.flashcards.server.flashcards.core.dto.TagDTO;
 
 import java.util.Map;
 import java.util.UUID;
 
 public interface CreateTagPort {
-    Map<String, String> createTag(CreateTagDTO createTagDTO, UUID userId);
+    TagDTO createTag(CreateTagDTO createTagDTO, UUID userId);
 }
