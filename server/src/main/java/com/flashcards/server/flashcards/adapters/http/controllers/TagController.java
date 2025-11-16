c93f53e9358cbb8605a8cfd5b0fe494580223d11
JaamalL <vitkt20@gmail.com>
2025-11-16 15:55:06 +0200

    feat(flashcards): Add POST /tags endpoint to expose creation handler for authorized users

--- server/src/main/java/com/flashcards/server/flashcards/adapters/http/controllers/TagController.java
+++ server/src/main/java/com/flashcards/server/flashcards/adapters/http/controllers/TagController.java
@@ -1,4 +1,34 @@
 package com.flashcards.server.flashcards.adapters.http.controllers;
 
+import com.flashcards.server.common.annotation.Authorize;
+import com.flashcards.server.flashcards.adapters.http.handlers.CreateTagHandler;
+import com.flashcards.server.flashcards.core.dto.CreateTagDTO;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("tags")
 public class TagController {
+    private final CreateTagHandler createTagHandler;
+
+    public TagController(CreateTagHandler createTagHandler) {
+        this.createTagHandler = createTagHandler;
+    }
+
+    @Authorize
+    @PostMapping
+    public ResponseEntity<Map<String, String>> create(
+            @AuthenticationPrincipal Jwt jwt,
+            @RequestBody @Valid CreateTagDTO createTagDTO
+    ) {
+        return createTagHandler.handle(jwt, createTagDTO);
+    }
 }
