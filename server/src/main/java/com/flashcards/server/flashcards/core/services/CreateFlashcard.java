9459c4d74627e99775f9d3711941cc23aa74ea7e
JaamalL <vitkt20@gmail.com>
2025-11-16 15:48:17 +0200

    feat(flashcards): Add tags association logic in CreateFlashcard service

--- server/src/main/java/com/flashcards/server/flashcards/core/services/CreateFlashcard.java
+++ server/src/main/java/com/flashcards/server/flashcards/core/services/CreateFlashcard.java
@@ -1,31 +1,53 @@
 package com.flashcards.server.flashcards.core.services;
 
+import com.flashcards.server.common.error.ApiError;
+import com.flashcards.server.common.exceptions.ApiException;
 import com.flashcards.server.flashcards.core.dto.CreateFlashcardDTO;
 import com.flashcards.server.flashcards.core.entities.Flashcard;
+import com.flashcards.server.flashcards.core.entities.Tag;
 import com.flashcards.server.flashcards.core.ports.repository.FlashcardRepositoryPort;
+import com.flashcards.server.flashcards.core.ports.repository.TagRepositoryPort;
 import com.flashcards.server.flashcards.core.ports.services.CreateFlashcardPort;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
 @Service
 public class CreateFlashcard implements CreateFlashcardPort {
     private final FlashcardRepositoryPort flashcardRepository;
+    private final TagRepositoryPort tagRepositoryPort;
 
-    public CreateFlashcard(FlashcardRepositoryPort flashcardRepository) {
+    public CreateFlashcard(FlashcardRepositoryPort flashcardRepository, TagRepositoryPort tagRepositoryPort) {
         this.flashcardRepository = flashcardRepository;
+        this.tagRepositoryPort = tagRepositoryPort;
     }
 
     @Override
     public Map<String, String> createFlashcard(CreateFlashcardDTO createFlashcardDTO, UUID userId) {
+        List<Tag> tags = tagRepositoryPort.findAllById(createFlashcardDTO.tagIds());
+
+        if (tags.size() != createFlashcardDTO.tagIds().size()) {
+            throw new ApiException(new ApiError(
+                    HttpStatus.BAD_REQUEST,
+                    "INVALID_TAG_ID",
+                    "One or more provided tag IDs are invalid or non-existent"
+            ));
+        }
+
         Flashcard entity = new Flashcard(
                 userId,
                 createFlashcardDTO.textQuestion(),
                 createFlashcardDTO.urlQuestion(),
                 createFlashcardDTO.answer()
         );
+
+        for (Tag tag : tags) {
+            entity.addTag(tag);
+        }
 
         flashcardRepository.create(entity);
 
