f07beea358860f346b603d6d72022e93e9b5b7d7
JaamalL <vitkt20@gmail.com>
2025-11-16 16:05:58 +0200

    chore: Remove unnecessary comments from docker-compse.sh script

--- scripts/docker-compose.sh
+++ scripts/docker-compose.sh
@@ -6,7 +6,4 @@ PROJECT_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
 DOCKER_DIR="$PROJECT_ROOT/docker"
 DOCKER_DATA_DIR="$DOCKER_DIR/data"
 
-# mkdir -p "$DOCKER_DATA_DIR"
-# chmod -R 777 "$DOCKER_DATA_DIR"
-
 docker compose -f "$DOCKER_DIR/docker-compose.yml" up -d --build
