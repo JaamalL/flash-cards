635a7d76d4cdb3d27112bcdf453c7e123956a230
JaamalL <vitkt20@gmail.com>
2025-11-16 15:50:16 +0200

    feat(flashcards): Implement CreateTagPort methods in CreateTag service

--- server/src/main/java/com/flashcards/server/flashcards/core/services/CreateTag.java
+++ server/src/main/java/com/flashcards/server/flashcards/core/services/CreateTag.java
@@ -1,4 +1,38 @@
 package com.flashcards.server.flashcards.core.services;
 
-public class CreateTag {
+import com.flashcards.server.flashcards.core.dto.CreateTagDTO;
+import com.flashcards.server.flashcards.core.entities.Tag;
+import com.flashcards.server.flashcards.core.ports.repository.TagRepositoryPort;
+import com.flashcards.server.flashcards.core.ports.services.CreateTagPort;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+@Service
+public class CreateTag implements CreateTagPort {
+    private final TagRepositoryPort tagRepositoryPort;
+
+    public CreateTag(TagRepositoryPort tagRepositoryPort) {
+        this.tagRepositoryPort = tagRepositoryPort;
+    }
+
+    @Override
+    public Map<String, String> createTag(CreateTagDTO createTagDTO, UUID userId) {
+        Tag entity = new Tag(
+                userId,
+                createTagDTO.name(),
+                createTagDTO.description()
+        );
+
+        tagRepositoryPort.create(entity);
+
+        Map<String, String> result = new HashMap<>();
+        result.put("id", entity.getId().toString());
+        result.put("name", entity.getName());
+        result.put("description", entity.getDescription());
+
+        return result;
+    }
 }
