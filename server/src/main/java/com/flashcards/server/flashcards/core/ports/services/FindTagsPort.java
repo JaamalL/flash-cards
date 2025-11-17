7af277fd89a167baf62d1693e02fd012e384ee09
JaamalL <vitkt20@gmail.com>
2025-11-17 20:55:40 +0200

    feat(flashcards): Add FindTagsPort to search tags by criteria

---
+++ server/src/main/java/com/flashcards/server/flashcards/core/ports/services/FindTagsPort.java
@@ -0,0 +1,12 @@
+package com.flashcards.server.flashcards.core.ports.services;
+
+import com.flashcards.server.flashcards.core.dto.TagDTO;
+import com.flashcards.server.flashcards.core.dto.TagDetailsDTO;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface FindTagsPort {
+    List<TagDTO> findByUserId(UUID userId);
+    TagDetailsDTO findById(UUID tagId);
+}
