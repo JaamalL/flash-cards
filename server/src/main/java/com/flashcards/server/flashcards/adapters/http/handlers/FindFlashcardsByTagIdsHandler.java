2f18edb69b0a7ca46461560ffa965bfd7a116f78
JaamalL <vitkt20@gmail.com>
2025-11-18 20:01:54 +0200

    feat(flashcards): Add FindFlashcardsByTagIdsHandler to handle findByTagIds service method

---
+++ server/src/main/java/com/flashcards/server/flashcards/adapters/http/handlers/FindFlashcardsByTagIdsHandler.java
@@ -0,0 +1,28 @@
+package com.flashcards.server.flashcards.adapters.http.handlers;
+
+import com.flashcards.server.flashcards.core.dto.FlashcardDTO;
+import com.flashcards.server.flashcards.core.enums.TagMatchMode;
+import com.flashcards.server.flashcards.core.ports.services.FlashcardQueryPort;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.UUID;
+
+@Component
+public class FindFlashcardsByTagIdsHandler {
+    private final FlashcardQueryPort flashcardQueryPort;
+
+    public FindFlashcardsByTagIdsHandler(FlashcardQueryPort flashcardQueryPort) {
+        this.flashcardQueryPort = flashcardQueryPort;
+    }
+
+    public ResponseEntity<List<FlashcardDTO>> handle(Jwt jwt, List<UUID> tagIds, TagMatchMode tagMatchMode) {
+        return ResponseEntity.ok(flashcardQueryPort.findByTagIds(
+                UUID.fromString(jwt.getSubject()),
+                tagIds,
+                tagMatchMode
+        ));
+    }
+}
