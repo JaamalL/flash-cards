0da80c7da59b82d081e89081b7263cfa90b5d581
JaamalL <vitkt20@gmail.com>
2025-11-18 22:01:18 +0200

    feat(flashcards): Add TagRepositoryPort findByUserIdAndId method to perform search tag by ID for specific user

--- server/src/main/java/com/flashcards/server/flashcards/core/ports/repository/TagRepositoryPort.java
+++ server/src/main/java/com/flashcards/server/flashcards/core/ports/repository/TagRepositoryPort.java
@@ -4,10 +4,12 @@ import com.flashcards.server.common.data.repository.IBaseRepository;
 import com.flashcards.server.flashcards.core.entities.Tag;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface TagRepositoryPort extends IBaseRepository<Tag> {
     List<Tag> findByUserId(UUID userId);
+    Optional<Tag> findByUserIdAndId(UUID userId, UUID tagId);
     List<Tag> findByIds(List<UUID> tagIds);
     long countByUserIdAndIds(UUID userId, List<UUID> tagIds);
 }
