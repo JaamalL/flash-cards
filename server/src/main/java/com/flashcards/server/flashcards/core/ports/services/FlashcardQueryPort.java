7956b560eab9dd54515d16ec0198af655e95b9cb
JaamalL <vitkt20@gmail.com>
2025-11-18 19:59:37 +0200

    feat(flashcards): Add FlashcardQueryPort findByTagIds method to perform searching flashcards by tags

---
+++ server/src/main/java/com/flashcards/server/flashcards/core/ports/services/FlashcardQueryPort.java
@@ -0,0 +1,11 @@
+package com.flashcards.server.flashcards.core.ports.services;
+
+import com.flashcards.server.flashcards.core.dto.FlashcardDTO;
+import com.flashcards.server.flashcards.core.enums.TagMatchMode;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface FlashcardQueryPort {
+    List<FlashcardDTO> findByTagIds(UUID uuid, List<UUID> tagIds, TagMatchMode tagMatchMode);
+}
