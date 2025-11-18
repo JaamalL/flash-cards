ce8e0129181642ae7f72c3251efec6ab34623c30
JaamalL <vitkt20@gmail.com>
2025-11-18 20:04:02 +0200

    feat(flashcards): Add GET /flashcards endpoint with query parameters to expose FindFlashcardsByTagIdsHandler

--- server/src/main/java/com/flashcards/server/flashcards/adapters/http/controllers/FlashcardController.java
+++ server/src/main/java/com/flashcards/server/flashcards/adapters/http/controllers/FlashcardController.java
@@ -2,26 +2,32 @@ package com.flashcards.server.flashcards.adapters.http.controllers;
 
 import com.flashcards.server.common.annotation.Authorize;
 import com.flashcards.server.flashcards.adapters.http.handlers.CreateFlashcardHandler;
+import com.flashcards.server.flashcards.adapters.http.handlers.FindFlashcardsByTagIdsHandler;
 import com.flashcards.server.flashcards.core.dto.CreateFlashcardDTO;
 import com.flashcards.server.flashcards.core.dto.FlashcardDTO;
+import com.flashcards.server.flashcards.core.enums.TagMatchMode;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.jwt.Jwt;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("flashcards")
 public class FlashcardController {
     private final CreateFlashcardHandler createFlashcardHandler;
+    private final FindFlashcardsByTagIdsHandler findFlashcardsByTagIdsHandler;
 
-    public FlashcardController(CreateFlashcardHandler createFlashcardHandler) {
+    public FlashcardController(
+            CreateFlashcardHandler createFlashcardHandler,
+            FindFlashcardsByTagIdsHandler findFlashcardsByTagIdsHandler
+    ) {
         this.createFlashcardHandler = createFlashcardHandler;
+        this.findFlashcardsByTagIdsHandler = findFlashcardsByTagIdsHandler;
     }
 
     @Authorize
@@ -31,5 +37,15 @@ public class FlashcardController {
             @RequestBody @Valid CreateFlashcardDTO createFlashcardDTO
     ) {
         return createFlashcardHandler.handle(jwt, createFlashcardDTO);
+    }
+
+    @Authorize
+    @GetMapping
+    ResponseEntity<List<FlashcardDTO>> getByTagIds(
+            @AuthenticationPrincipal Jwt jwt,
+            @RequestParam(name = "matchMode", defaultValue = "ALL") TagMatchMode tagMatchMode,
+            @RequestParam(name = "tagId") List<UUID> tagIds
+    ) {
+        return findFlashcardsByTagIdsHandler.handle(jwt, tagIds, tagMatchMode);
     }
 }
