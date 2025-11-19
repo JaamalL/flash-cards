13821ac97009620d6a4230a8d165b5a883c867ee
JaamalL <vitkt20@gmail.com>
2025-11-19 13:56:51 +0200

    feat(flashcards): Add FlashcardQueryPort getByUserId and getById methods to perform get flashcard requests for specific user

--- server/src/main/java/com/flashcards/server/flashcards/core/ports/services/FlashcardQueryPort.java
+++ server/src/main/java/com/flashcards/server/flashcards/core/ports/services/FlashcardQueryPort.java
@@ -1,11 +1,14 @@
 package com.flashcards.server.flashcards.core.ports.services;
 
 import com.flashcards.server.flashcards.core.dto.FlashcardDTO;
+import com.flashcards.server.flashcards.core.dto.FlashcardDetailsDTO;
 import com.flashcards.server.flashcards.core.enums.TagMatchMode;
 
 import java.util.List;
 import java.util.UUID;
 
 public interface FlashcardQueryPort {
+    List<FlashcardDTO> getByUserId(UUID userId);
+    FlashcardDetailsDTO getById(UUID userId, UUID flashcardId);
     List<FlashcardDTO> getByTagIds(UUID uuid, List<UUID> tagIds, TagMatchMode tagMatchMode);
 }
