d12456c55e3bcdcbf075d288c825f78148f6fda7
JaamalL <vitkt20@gmail.com>
2025-11-19 13:49:40 +0200

    feat(flashcards): Add FlashcardRepositoryPort findByUserIdAndId method to perform search flashcard by ID for specific user

--- server/src/main/java/com/flashcards/server/flashcards/core/ports/repository/FlashcardRepositoryPort.java
+++ server/src/main/java/com/flashcards/server/flashcards/core/ports/repository/FlashcardRepositoryPort.java
@@ -2,12 +2,15 @@ package com.flashcards.server.flashcards.core.ports.repository;
 
 import com.flashcards.server.common.data.repository.IBaseRepository;
 import com.flashcards.server.flashcards.core.entities.Flashcard;
+import com.flashcards.server.flashcards.core.entities.Tag;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface FlashcardRepositoryPort extends IBaseRepository<Flashcard> {
     List<Flashcard> findByUserId(UUID userId);
+    Optional<Flashcard> findByUserIdAndId(UUID userId, UUID flashcardId);
     List<Flashcard> findByUserIdAndAnyTagIds(UUID userId, List<UUID> tagIds);
     List<Flashcard> findByUserIdAndAllTagIds(UUID userId, List<UUID> tagIds);
 }
