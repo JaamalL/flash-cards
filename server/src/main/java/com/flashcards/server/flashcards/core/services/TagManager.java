5b1e4f567483a2942d98182eb1e6d189f723924c
JaamalL <vitkt20@gmail.com>
2025-11-20 12:12:01 +0200

    feat(flashcards): Implement TagManagerPort deleteById method

--- server/src/main/java/com/flashcards/server/flashcards/core/services/TagManager.java
+++ server/src/main/java/com/flashcards/server/flashcards/core/services/TagManager.java
@@ -1,11 +1,14 @@
 package com.flashcards.server.flashcards.core.services;
 
+import com.flashcards.server.common.error.ApiError;
+import com.flashcards.server.common.exceptions.ApiException;
 import com.flashcards.server.flashcards.core.dto.CreateTagDTO;
 import com.flashcards.server.flashcards.core.dto.TagDTO;
 import com.flashcards.server.flashcards.core.dto.TagDetailsDTO;
 import com.flashcards.server.flashcards.core.entities.Tag;
 import com.flashcards.server.flashcards.core.ports.repository.TagRepositoryPort;
 import com.flashcards.server.flashcards.core.ports.services.TagManagerPort;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -36,5 +39,17 @@ public class TagManager implements TagManagerPort {
                 new ArrayList<>(),
                 entity.getCreatedAt().toString()
         );
+    }
+
+    @Override
+    public void deleteById(UUID userId, UUID tagId) {
+        if (tagRepositoryPort.deleteByUserIdAndId(userId, tagId) == 0) {
+            throw new ApiException(new ApiError(
+                    HttpStatus.NOT_FOUND,
+                    "NOT_FOUND",
+                    Tag.class.getSimpleName() + " with id " + tagId +
+                            " not found for user with " + userId + " id"
+            ));
+        }
     }
 }
