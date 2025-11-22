f63faeb7d944c2735b07ffa9d76dc7bcaaf8ba45
JaamalL <vitkt20@gmail.com>
2025-11-22 13:32:55 +0200

    feat(flashcards): Add update methods to FlashcardManagerPort and TagManagerPort

--- server/src/main/java/com/flashcards/server/flashcards/core/ports/services/FlashcardManagerPort.java
+++ server/src/main/java/com/flashcards/server/flashcards/core/ports/services/FlashcardManagerPort.java
@@ -1,12 +1,11 @@
 package com.flashcards.server.flashcards.core.ports.services;
 
-import com.flashcards.server.flashcards.core.dto.CreateFlashcardDTO;
-import com.flashcards.server.flashcards.core.dto.FlashcardDTO;
-import com.flashcards.server.flashcards.core.dto.FlashcardDetailsDTO;
+import com.flashcards.server.flashcards.core.dto.*;
 
 import java.util.UUID;
 
 public interface FlashcardManagerPort {
     FlashcardDetailsDTO create(CreateFlashcardDTO createFlashcardDTO, UUID userId);
+    FlashcardDetailsDTO update(UUID userId, UUID flashcardId, UpdateFlashcardDTO updateFlashcardDTO);
     void deleteById(UUID userId, UUID flashcardId);
 }
--- server/src/main/java/com/flashcards/server/flashcards/core/ports/services/TagManagerPort.java
+++ server/src/main/java/com/flashcards/server/flashcards/core/ports/services/TagManagerPort.java
@@ -3,10 +3,12 @@ package com.flashcards.server.flashcards.core.ports.services;
 import com.flashcards.server.flashcards.core.dto.CreateTagDTO;
 import com.flashcards.server.flashcards.core.dto.TagDTO;
 import com.flashcards.server.flashcards.core.dto.TagDetailsDTO;
+import com.flashcards.server.flashcards.core.dto.UpdateTagDTO;
 
 import java.util.UUID;
 
 public interface TagManagerPort {
     TagDetailsDTO create(CreateTagDTO createTagDTO, UUID userId);
+    TagDetailsDTO update(UUID userId, UUID tagId, UpdateTagDTO updateTagDTO);
     void deleteById(UUID userId, UUID tagId);
 }
